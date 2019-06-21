c797c58c1425ce816b3bced86b6bc055da2260d4
Pablo Castillo <pablo@castillo.is>
2019-06-21 13:35:54 -0700

    Added rancher-cli v 0.6.13

---
+++ rancher-cli@0.6.rb
@@ -0,0 +1,23 @@
+class RancherCliAT06 < Formula
+  desc "The Rancher CLI is a unified tool to manage your Rancher server"
+  homepage "https://github.com/rancher/cli"
+  url "https://github.com/rancher/cli/archive/v0.6.13.tar.gz"
+  sha256 "360c7f79ce8e60da821cb07254860a8f4ae1492760a2f606325713d20efbcd14"
+
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/rancher/cli/").install Dir["*"]
+    system "go", "build", "-ldflags",
+           "-w -X github.com/rancher/cli/version.VERSION=#{version}",
+           "-o", "#{bin}/rancher",
+           "-v", "github.com/rancher/cli/"
+  end
+
+  test do
+    system bin/"rancher", "help"
+  end
+end
